54b7d522394a286e5c78c93d9996d388df7fb8b2
Roushan <kumar7roushanjha2005@gmail.com>
2024-12-11 10:02:27 +0530

    refactor: enhance booking controller with asyncHandler for improved error handling and response consistency

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,33 +1,27 @@
-import Booking from '../models/Booking.js'
+import Booking from '../models/Booking.js';
+import asyncHandler from "../utils/asyncHandler.js";
 
 // Create a new booking
-const createBooking = async (req, res) => {
-  try {
-    const { name, email, phone, date, time, people } = await req.body;
-    // console
-    if (!name || !email || !phone || !date || !time || !people) {
-      return res.status(400).json({ message: "All fields are required" ,sucess: false});
-    }
-    const booking = new Booking({ name, email, phone, date, time, people });
-    await booking.save();
-    res.status(201).json({ message: "Booking created successfully", booking , sucess: true});
-    }
-    catch (error) {
-      res.status(500).json({ error: error.message ,sucess: false});
-    }
-}
+const createBooking = asyncHandler(async (req, res) => {
+  const { name, email, phone, date, time, people } = req.body;
 
-const getBookingById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const booking = await Booking
-            .findById(id);
-        if (!booking) { return res.status(404).json({ message: "Booking not found",sucess: false }); }
-        res.status(200).json({ booking, sucess: true });
-    }
-    catch (error) {
-        res.status(500).json({ error: error.message, sucess: false });
-    }
-};
+  if (!name || !email || !phone || !date || !time || !people) {
+    return res.status(400).json({ message: "All fields are required", success: false });
+  }
+
+  const booking = new Booking({ name, email, phone, date, time, people });
+  await booking.save();
+  res.status(201).json({ message: "Booking created successfully", booking, success: true });
+});
+
+// Get booking by ID
+const getBookingById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const booking = await Booking.findById(id);
+  if (!booking) {
+    return res.status(404).json({ message: "Booking not found", success: false });
+  }
+  res.status(200).json({ booking, success: true });
+});
 
 export { createBooking, getBookingById };
