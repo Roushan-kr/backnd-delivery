131272398dd718a6508bdb75b0725d1e9403fbd2
Roushan <kumar7roushanjha2005@gmail.com>
2024-12-12 12:00:05 +0530

    update

--- index.js
+++ index.js
@@ -31,9 +31,12 @@ connectDB()
   .then(() => {
     console.log(`Database connected successfully.`);
     // No need to use app.listen in serverless environments
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
   })
   .catch((error) => {
     console.log(`Error: ${error.message}`);
   });
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
